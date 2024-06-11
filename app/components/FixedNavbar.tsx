fe757f5bd6ba8f0a86cef99602baf3b4a3b2f2ed
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-06-11 11:42:26 +0530

    nav scale duration

--- app/components/FixedNavbar.tsx
+++ app/components/FixedNavbar.tsx
@@ -52,11 +52,16 @@ const FixedNavbar = ({ fullNav }: any) => {
             scaleX: '100%',
             transition: {
                 ease: 'easeOut',
-                duration: 0.5
+                layout: { duration: 0.2 }
+
             }
         },
         exit: {
-            scaleX: '70%',
+            scaleX: '0%',
+            transition: {
+                type: 'spring',
+                duration: 0.5
+            }
         }
     }
     // const ref = useRef(null)
@@ -68,6 +73,8 @@ const FixedNavbar = ({ fullNav }: any) => {
 
     return (
         <motion.div key='nav' initial='initial' variants={NavAnimations} animate='animate' className='w-auto fixed  h-auto left-1/2 top-6 opacity-1 z-[7] '>
+
+
             {!fullNav ?
                 <div className="w-auto   self-stretch h-min flex flex-none justify-center">
                     <div className="h-auto w-auto relative flex-none z-[5] bg-[#232323]  rounded-[100px]  ">
@@ -83,8 +90,8 @@ const FixedNavbar = ({ fullNav }: any) => {
                 </div>
 
                 :
-                <motion.div key='startNav' exit='exit' variants={NavAnimations} initial='NavInitial' animate='Nav' className=" flex gap-3 justify-center items-center px-6 py-3 w-auto h-[43px] [box-shadow:rgba(0, 0, 0, 0.15)_8px_10px_15px_0px] bg-[#232323] text-[#fff]  rounded-[100px] ">
-                    <AnimatePresence>
+                <AnimatePresence>
+                    <motion.div layout key='startNav' exit='exit' variants={NavAnimations} initial='NavInitial' animate='Nav' className=" flex gap-3 justify-center items-center px-6 py-3 w-auto h-[43px] [box-shadow:rgba(0, 0, 0, 0.15)_8px_10px_15px_0px] bg-[#232323] text-[#fff]  rounded-[100px] ">
                         <motion.div layout variants={NavAnimations} initial='startInitial' animate='start' className='order-10 text-[#f1fd82]' >
                             <a href="" className='flex flex-none items-center gap-2 h-10 '>
                                 <div><p>Start Free</p></div>
@@ -93,33 +100,33 @@ const FixedNavbar = ({ fullNav }: any) => {
                                 </svg></defs><use href="#svg286516480_576"></use></svg></div>
                             </a>
                         </motion.div>
-                    </AnimatePresence>
-                    <div>
-                        <a href="" className='flex items-center flex-none gap-2'>
-                            <div className="bg-[#f1fd82] rounded-[100%] w-2 aspect-square relative flex-none"></div>
-                            <motion.div variants={NavAnimations} initial='linksInitial' animate='links'>
-                                <p>Product</p>
-                            </motion.div>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="">
-                            <motion.div variants={NavAnimations} initial='linksInitial' animate='links'>
-                                <p>Pricing</p>
-                            </motion.div>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="">
-                            <motion.div variants={NavAnimations} initial='linksInitial' animate='links'>
-                                <p>
-                                    Login
-                                </p>
-                            </motion.div>
-                        </a>
-                    </div>
-                </motion.div>
+                        <div>
+                            <a href="" className='flex items-center flex-none gap-2'>
+                                <div className="bg-[#f1fd82] rounded-[100%] w-2 aspect-square relative flex-none"></div>
+                                <motion.div variants={NavAnimations} initial='linksInitial' animate='links'>
+                                    <p>Product</p>
+                                </motion.div>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <motion.div variants={NavAnimations} initial='linksInitial' animate='links'>
+                                    <p>Pricing</p>
+                                </motion.div>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <motion.div variants={NavAnimations} initial='linksInitial' animate='links'>
+                                    <p>
+                                        Login
+                                    </p>
+                                </motion.div>
+                            </a>
+                        </div>
+                    </motion.div>
 
+                </AnimatePresence>
             }
         </motion.div>
     )
