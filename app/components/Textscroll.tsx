d3ce4c7c142adaa97c05994424d63830998628de
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-06-13 07:50:54 +0530

    text scroll correction

--- app/components/Textscroll.tsx
+++ app/components/Textscroll.tsx
@@ -1,6 +1,7 @@
 'use client'
-import { motion, useScroll, useTransform } from "framer-motion"
+import { color, motion, useScroll, useTransform } from "framer-motion"
 import React, { useEffect, useRef } from "react"
+import { myFont, myFontBook } from "../staticFonts/fonts"
 
 
 const Textscroll = () => {
@@ -19,31 +20,37 @@ const Textscroll = () => {
     const words = paragraph.split(' ')
     return (
         <>
-            <div ref={element} className=" rounded-l-[50px] h-[300vh] rounded-r-[50px] mx-6 px-6  py-0 bg-[#111] z-[6] ">
-                <div className="sticky top-12 flex pt-56 px-20 pb-20 flex-none flex-wrap items-center mx-auto text-[40px] text-gray-300">
-                    {
-                        words.map((word, i) => {
-                            const start = i / words.length
-                            const end = start + 1 / words.length
-                            // console.log(start, end);
+            <div ref={element} className=" rounded-l-[50px] h-[300vh] w-full rounded-r-[50px] mx-6 px-6  py-0 bg-[#111] z-[6] ">
+                <div className="flex flex-none sticky top-0 h-screen w-full justify-center items-center" >
 
-                            return <Word key={i} range={[start, end]} progress={scrollYProgress}>{word}</Word>
-                        })
-                    }
+                    <div className={` w-[50%] text-[45px] ${myFontBook.variable} font-anthroBook sticky max-w-[971px] top-[25vh] flex flex-wrap flex-none  items-center mx-auto   leading-[1] z-[1] p-0 break-words whitespace-normals `}>
+                        {
+                            words.map((word, i) => {
+                                const start = i / words.length
+                                const end = start + 1 / words.length
+                                // console.log(start, end);
+
+                                return <Word key={i} range={[start, end]} progress={scrollYProgress}>{word}</Word>
+                            })
+                        }
+                    </div>
                 </div>
             </div>
-            <div className="h-screen"></div>
+
         </>
     )
 }
 
 const Word = ({ children, range, progress }: any) => {
-    const opacity = useTransform(progress, range, [0, 1])
+    const colorValue = useTransform(progress, range, ['#737373', '#ffffff'])
+    // console.log(colorValue);
+
+
     return (
-        <div className="mr-3 mt-3 relative">
-            <p className="opacity-30 absolute" >{children}</p>
-            <motion.p style={{ opacity }} className="" >{children}</motion.p>
-        </div>
+        <div className="mr-3 mt-3 relative text-[#737373] ">
+            {/* <p className=" absolute " >{children}</p> */}
+            <motion.p style={{ color: colorValue }}   >{children}</motion.p>
+        </div >
     )
 }
 
