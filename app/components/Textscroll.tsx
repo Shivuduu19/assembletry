b2da9472a177ee1070c0f4bc3504becd253512a2
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-06-13 11:15:55 +0530

    add textscroll responsivity

--- app/components/Textscroll.tsx
+++ app/components/Textscroll.tsx
@@ -6,7 +6,7 @@ import { myFont, myFontBook } from "../staticFonts/fonts"
 
 const Textscroll = () => {
     const element = useRef(null)
-    const paragraph = `At some point along the way project management tools got too complex  instead of simplifying work  they did the opposite So we stripped it all away  and chose to organize everything by the most basic fundamental concept: time 
+    const paragraph = `At some point along the way project management tools got too complex  instead of simplifying work they did the opposite So we stripped it all away  and chose to organize everything by the most basic fundamental concept: time 
  `
     const { scrollYProgress } = useScroll({
         target: element,
@@ -20,10 +20,10 @@ const Textscroll = () => {
     const words = paragraph.split(' ')
     return (
         <>
-            <div ref={element} className=" rounded-l-[50px] h-[300vh] w-full rounded-r-[50px] mx-6 px-6  py-0 bg-[#111] z-[6] ">
-                <div className="flex flex-none sticky top-0 h-screen w-full justify-center items-center" >
+            <div ref={element} className=" rounded-l-[50px] h-[300vh] w-full rounded-r-[50px]  sm:mx-6 px-2 sm:px-6  flex flex-none justify-center py-0 bg-[#111] z-[6] ">
+                <div className="flex flex-none sticky top-0 h-screen w-full  items-center" >
 
-                    <div className={` w-[50%] text-[45px] ${myFontBook.variable} font-anthroBook sticky max-w-[971px] top-[25vh] flex flex-wrap flex-none  items-center mx-auto   leading-[1] z-[1] p-0 break-words whitespace-normals `}>
+                    <div className={`  text-[24px] sm:text-[45px] ${myFontBook.variable} font-anthroBook sticky sm:max-w-[971px] top-[25vh] flex flex-wrap flex-none w-[95%] max-w-[450px] items-center mx-auto leading-[0.8]   sm:leading-[1] z-[1] p-0 overf  `}>
                         {
                             words.map((word, i) => {
                                 const start = i / words.length
